fc4441e289c13064a1974230d7292c70efa6b2d5
object1037 <contact@object1037.dev>
2021-10-10 14:01:12 +0900

    resをfilter

--- pages/api/kaomoji/[pron].ts
+++ pages/api/kaomoji/[pron].ts
@@ -5,7 +5,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { pron } = req.query
   const encoded = encodeURI(`https://cloud.simeji.me/py?ol=1&switch=2&section=1&ver=10.6&api_version=2&web=1&py=${pron}`)
   try {
-    const results = await axios.get(encoded).then(res => res.data)
+    const results = await axios.get<any>(encoded)
+    .then(function(res) {
+      if (!res.data.data) {
+        return null
+      } else {
+        const filtered = res.data.data[0].candidates.filter((word: any) => word.type == 9)
+        return filtered
+      }
+    })
     return res.json(results)
   } catch (e: any) {
     res.status(500).json({ message: e.message })
