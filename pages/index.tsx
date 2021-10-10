18183aa429f15748450bb72b313dde3ffd597938
object1037 <contact@object1037.dev>
2021-10-10 12:19:57 +0900

    install tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Webで顔文字変換" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@object1037" />
-        <meta property="og:url" content="https://bnasimeji.object1037.dev" />
+        <meta property="og:url" content="https://bnasimeji.vercel.app" />
         <meta property="og:title" content="ぶなしめじ" />
         <meta property="og:description" content="Webで顔文字変換" />
         <meta property="og:image" content="https://object1037.dev/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
