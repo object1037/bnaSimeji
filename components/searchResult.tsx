9a724191a15ccb5337eef8a2a800de8320cb9e85
object1037 <contact@object1037.dev>
2021-10-10 20:49:39 +0900

    update result view
    2れつにする

--- components/searchResult.tsx
+++ components/searchResult.tsx
@@ -9,43 +9,65 @@ const SearchResult = memo(function SearchResult({
  }) {
   const { kaomojis, isLoading, isError } = useKaomojis(pron)
 
-  if (isLoading) {
-    return null
-  }
-  if (isError) {
-    return <p>エラー</p>
-  }
-
   const resultsWrapperStyle = [
     'border-t',
     'border-gray-300',
     'my-12',
     'flex',
-    'max-w-md',
+    'max-w-3xl',
     'mx-auto',
   ]
+  const listWrapperStyle = [
+    'w-full',
+    'flex',
+    'flex-wrap'
+  ]
   const listStyle = [
+    'truncate',
     'px-3',
     'py-4',
     'w-full',
+    'md:w-1/2',
     'border-b',
     'border-gray-300'
   ]
+  const infoStyle = [
+    'py-4',
+    'text-center',
+    'border-b',
+    'text-gray-500',
+    'w-full'
+  ]
 
-  const noResult = <p className="py-4 text-center border-b text-gray-500">見つかりません</p>
+  if (isLoading) {
+    return (
+      <div className={clsx(resultsWrapperStyle)}>
+        <p className={clsx(infoStyle)}>Loading...</p>
+      </div>
+    )
+  }
+  if (isError) {
+    return (
+      <div className={clsx(resultsWrapperStyle)}>
+        <p className={clsx(infoStyle)}>ひらがな2文字以上で入力して乁(˙꒳˙乁)ｸﾚﾖ...</p>
+      </div>
+    )
+  }
+
+  const noResult = <p className={clsx(infoStyle)}>見つかりません …ﾄﾎﾎ( ×ω× ;)</p>
 
   return (
     <div className={clsx(resultsWrapperStyle)}>
-      <ul className="w-full">
+      <div className={clsx(listWrapperStyle)}>
         {kaomojis!.length === 0 ? noResult : 
         kaomojis!.map((kaomoji) => {
           return (
-            <li key={kaomoji.word} className={clsx(listStyle)}>
+            <div key={kaomoji.word} className={clsx(listStyle)}>
               {kaomoji.word}
-            </li>
+            </div>
           )
         })}
-      </ul>
+      </div>
     </div>
   )
 })
