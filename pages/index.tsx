ff17db0333a577c962336f2814cb9682e4846d63
object1037 <contact@object1037.dev>
2021-10-10 14:59:09 +0900

    polish input style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import SearchResult from '../components/searchResult'
+import { FiSearch } from 'react-icons/fi'
+import clsx from 'clsx'
 
 const Home: NextPage = () => {
   const [pron, setPron] = useState('')
@@ -11,6 +13,19 @@ const Home: NextPage = () => {
     e.preventDefault()
     setPron(inputValue)
   }
+
+  const buttonStyle = [
+    'bg-gray-900',
+    'text-gray-100',
+    'rounded-md',
+    'border',
+    'border-gray-900',
+    'p-3',
+    'text-xl',
+    'hover:bg-transparent',
+    'hover:text-gray-900',
+    'transition'
+  ]
 
   return (
     <>
@@ -25,15 +40,18 @@ const Home: NextPage = () => {
         <meta property="og:description" content="Webで顔文字変換" />
         <meta property="og:image" content="https://object1037.dev/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
       </Head>
-      <main>
-        <form onSubmit={submitHandler}>
+      <main className="px-6">
+        <form onSubmit={submitHandler} className="mt-16 flex justify-center space-x-3 max-w-md mx-auto">
           <input
             aria-label="search kaomoji"
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            className="rounded-md flex-grow"
           />
-          <button type="submit">検索</button>
+          <button type="submit" className={clsx(buttonStyle)}>
+            <FiSearch />
+          </button>
         </form>
         {pron && <SearchResult pron={pron}/>}
       </main>
