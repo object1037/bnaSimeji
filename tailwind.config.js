5f471adfd2a3ca5afdb790d334abe01781b9f77f
object1037 <contact@object1037.dev>
2022-04-27 00:24:50 +0900

    update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        gray: colors.trueGray,
-      },
-    },
-  },
   variants: {
     extend: {},
   },
