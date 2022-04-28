f0d58763e8e514cd12382fcd56866e7ee0213af5
object1037 <contact@object1037.dev>
2022-04-28 23:43:45 +0900

    update description

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,12 +12,12 @@ export const Layout = ({
     <Head>
       <title>ぶなしめじ</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="description" content="Webで顔文字変換" />
+      <meta name="description" content="WebでSimejiの顔文字変換" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content="@object1037" />
       <meta property="og:url" content="https://bnasimeji.vercel.app" />
       <meta property="og:title" content="ぶなしめじ" />
-      <meta property="og:description" content="Webで顔文字変換" />
+      <meta property="og:description" content="WebでSimejiの顔文字変換" />
       <meta property="og:image" content="https://object1037.dev/_next/image?url=%2Fimages%2Fprofile.jpg&w=640&q=75" />
     </Head>
     <main className="px-6">
